184a4ebad2514f61f861ab64f5f71318f9982b7b
2nytwin <2nytwinconnect@gmail.com>
2023-03-06 23:45:15 +0300

    1/6

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://shop.db'
+db = SQLAlchemy
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Integer, primary_key=True)
+    price = 0
+    isActive = 0
+    amount = 0
 
 
 @app.route('/')
