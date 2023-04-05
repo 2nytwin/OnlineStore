0d7bc3ebc95beff5daa003811d053941a673896c
2nytwin <2nytwinconnect@gmail.com>
2023-04-05 19:29:42 +0300

    comp

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://shop.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Integer, primary_key=True)
-    price = 0
-    isActive = 0
-    amount = 0
+    title = db.Column(db.String(100), nullabel=False)
+    price = db.Column(db.Integer, nullabel=False)
+    isActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return self.title
 
 
 @app.route('/')
@@ -22,6 +25,24 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
+    else:
+        return render_template('create.html')
 
 
 if __name__ == '__main__':
